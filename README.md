806fd313e6040356ae7332639116068338678041
luau-project <luau.project@gmail.com>
2024-05-01 19:14:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# minpack-builder
+
+## Table of Contents
+
+* [Introduction](#introduction)
+    * [Description](#description)
+    * [What is minpack?](#what-is-minpack)
+    * [Why minpack-builder is needed?](#why-minpack-builder-is-needed)
+    * [How to solve the problem?](#how-to-solve-the-problem)
+* [Supported Platforms](#supported-platforms)
+* [Usage](#usage)
+* [References](#references)
+
+## Introduction
+
+### Description
+
+The goal of **minpack-builder** is to provide an unified C API and a standard way to build-and-install binaries (*shared and/or static library in machine code*) of the double precision version of MINPACK-1 [[1]](https://www.netlib.org/minpack) - *or simply minpack*. In order to achieve this goal, we employ the powerful CMake [[2]](https://cmake.org/) build system to handle platform and compiler intricacies on different operating systems.
+
+### What is minpack?
+
+The original minpack [[1]](https://www.netlib.org/minpack) is a battle-tested **min**imization **pack**age written in Fortran 77 for solving a system of nonlinear equations and nonlinear least squares problems, discussed in details at [[3]](https://doi.org/10.2172/6997568). Throughout the years, popular scientific libraries like SciPy [[4]](https://github.com/scipy/scipy/tree/main/scipy/optimize/minpack) and Eigen [[5]](https://eigen.tuxfamily.org/dox/unsupported/index.html) have been using minpack to perform nonlinear optimization, either in unmodified form or rewritten to programming languages other than Fortran.
+
+### Why minpack-builder is needed?
+
+On a wide range of Linux operating systems [[6]](https://repology.org/project/minpack), due the widespread adoption of gfortran [[7]](https://gcc.gnu.org/wiki/GFortran) as robust and free option for a Fortran compiler, minpack can be installed by the official package manager of the distribution. On Windows, through the great MSYS2 ecosystem [[8]](https://www.msys2.org/) which keeps that feeling of the Linux world, I authored with contributions [[9]](https://github.com/msys2/MINGW-packages/pull/20643) the initial integration of minpack as a package on MSYS2 [[10]](https://packages.msys2.org/base/mingw-w64-libminpack) to deliver Windows developers access to native binaries compiled by the free Fortran compilers gfortran [[7]](https://gcc.gnu.org/wiki/GFortran) and LLVM Flang [[11]](https://flang.llvm.org/docs/).
+
+While MSYS2 is an excellent choice for people working with GCC-like toolchains, Microsoft Visual C/C++ (MSVC) build tools [[12]](https://visualstudio.microsoft.com/visual-cpp-build-tools) are extremely popular among Windows developers. Whenever someone is looking for a Fortran compiler (Intel® Fortran Compiler [[13]](https://www.intel.com/content/www/us/en/developer/tools/oneapi/fortran-compiler.html) and LLVM Flang [[11]](https://flang.llvm.org/docs/)) that integrates nicely with MSVC build tools, the developer will have to build minpack from the source code on its own. Moreover, Fortran compilers often export symbols with different naming conventions, leading to different C APIs if standard compiler options were applied.
+
+### How to solve the problem?
+
+In order to provide an unified C API for minpack, we use CMake [[2]](https://cmake.org/) as a build system to download the source code directly from the official minpack website [[1]](https://www.netlib.org/minpack). Then, we handle vendor-specific parameters of the Fortran compiler to build the library conforming to the API seen on Linux operating systems. Finally, we install it on a suitable location defined by the developer, making the minpack binaries easily discoverable as a CMake [[2]](https://cmake.org/) or pkg-config [[14]](https://gitlab.freedesktop.org/pkg-config/pkg-config) module.
+
+## Supported Platforms
+
+Under construction.
+
+## Usage
+
+Under construction.
+
+## References
+
+1. Official minpack website: [https://www.netlib.org/minpack](https://www.netlib.org/minpack);
+2. CMake: [https://cmake.org/](https://cmake.org/);
+3. Moré, Jorge J., Burton S. Garbow, and Kenneth E. Hillstrom. *User guide for MINPACK-1.[In FORTRAN]*. No. ANL-80-74. Argonne National Lab.(ANL), Argonne, IL (United States), 1980. DOI: [10.2172/6997568](https://doi.org/10.2172/6997568);
+4. minpack on SciPy: [https://github.com/scipy/scipy/tree/main/scipy/optimize/minpack](https://github.com/scipy/scipy/tree/main/scipy/optimize/minpack);
+5. minpack on Eigen: [https://eigen.tuxfamily.org/dox/unsupported/index.html](https://eigen.tuxfamily.org/dox/unsupported/index.html);
+6. minpack on Linux operating systems: [https://repology.org/project/minpack](https://repology.org/project/minpack);
+7. gfortran: [https://gcc.gnu.org/wiki/GFortran](https://gcc.gnu.org/wiki/GFortran);
+8. MSYS2 project: [https://www.msys2.org/](https://www.msys2.org/);
+9. minpack introduction on MSYS2: [https://github.com/msys2/MINGW-packages/pull/20643](https://github.com/msys2/MINGW-packages/pull/20643);
+10. minpack as a MSYS2 package: [https://packages.msys2.org/base/mingw-w64-libminpack](https://packages.msys2.org/base/mingw-w64-libminpack);
+11. LLVM Flang: [https://flang.llvm.org/docs/](https://flang.llvm.org/docs/);
+12. Microsoft Visual C/C++ (MSVC) build tools: [https://visualstudio.microsoft.com/visual-cpp-build-tools](https://visualstudio.microsoft.com/visual-cpp-build-tools);
+13. Intel® Fortran Compiler: [https://www.intel.com/content/www/us/en/developer/tools/oneapi/fortran-compiler.html](https://www.intel.com/content/www/us/en/developer/tools/oneapi/fortran-compiler.html);
+14. pkg-config: [https://gitlab.freedesktop.org/pkg-config/pkg-config](https://gitlab.freedesktop.org/pkg-config/pkg-config).
---
+++ cmake/DownloadMinpackSources.cmake
@@ -0,0 +1,146 @@
+#                                                                             #
+#                                                                             #
+###############################################################################
+# License                                                                     #
+###############################################################################
+#                                                                             #
+# Copyright (c) 2024 luau-project https://github.com/luau-project/minpackex   #
+#                                                                             #
+# Redistribution and use in source and binary forms, with or without          #
+# modification, are permitted provided that the following conditions are met: #
+#                                                                             #
+# 1. Redistributions of source code must retain the above copyright notice,   #
+# this list of conditions and the following disclaimer.                       #
+#                                                                             #
+# 2. Redistributions in binary form must reproduce the above copyright        #
+# notice, this list of conditions and the following disclaimer in the         #
+# documentation and/or other materials provided with the distribution.        #
+#                                                                             #
+# 3. Neither the name of the copyright holder nor the names of its            #
+# contributors may be used to endorse or promote products derived from this   #
+# software without specific prior written permission.                         #
+#                                                                             #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,       #
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR      #
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR           #
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,       #
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,         #
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; #
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,    #
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR     #
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF      #
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                  #
+#                                                                             #
+###############################################################################
+# Start(Description of function)                                              #
+###############################################################################
+#                                                                             #
+# [function] download_minpack_sources                                         #
+#                                                                             #
+#   [param] [in]                                                              #
+#    destination_dir: directory to store downloaded files from ${minpack_url} #
+#                                                                             #
+#   [param] [out-var]                                                         #
+#    minpack_source_files: LIST containing full paths of the downloaded       #
+#    fortran files                                                            #
+#                                                                             #
+#   [param] [out-var]                                                         #
+#    minpack_copyright: STRING containing a full path for the downloaded      #
+#    copyright file                                                           #
+#                                                                             #
+###############################################################################
+# End(Description of function)                                                #
+###############################################################################
+#                                                                             #
+#                                                                             #
+###############################################################################
+###############################################################################
+##                                 Body                                      ##
+###############################################################################
+###############################################################################
+#                                                                             #
+#                                                                             #
+
+function(download_minpack_sources destination_dir minpack_source_files minpack_copyright)
+
+    message(STATUS "")
+    message(STATUS " Starting download ...")
+    message(STATUS "")
+
+    get_minpack_sources(
+        # Fortran source basenames for minpack
+        minpack_source_basenames
+
+        # sha256 sums for each source Fortran file
+        minpack_sha256_sums_for_source_files
+
+        # basename copyright for minpack
+        minpack_copyright_basename
+
+        # sha256 sum for copyright
+        minpack_sha256_sum_for_copyright
+    )
+
+    set(minpack_copyright_file_url "${minpack_url}/${minpack_copyright_basename}")
+    set(minpack_copyright_file_destination "${destination_dir}/${minpack_copyright_basename}")
+
+    message(STATUS "")
+    if (EXISTS ${minpack_copyright_file_destination})
+        message(STATUS " Found copyright file (${minpack_copyright_basename}) at:")
+        message(STATUS "     ${minpack_copyright_file_destination}")
+    else()
+        message(STATUS " Downloading copyright file (${minpack_copyright_basename})")
+        message(STATUS "     with expected SHA256 sum ${minpack_sha256_sum_for_copyright}")
+
+        file(DOWNLOAD
+            ${minpack_copyright_file_url}
+            ${minpack_copyright_file_destination}
+            EXPECTED_HASH SHA256=${minpack_sha256_sum_for_copyright}
+            STATUS DOWNLOAD_STATUS
+        )
+
+        list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
+        if (NOT (${STATUS_CODE} EQUAL 0))
+            list(GET DOWNLOAD_STATUS 1 ERROR_MESSAGE)
+            message(FATAL_ERROR "Failed to download minpack copyright: ${ERROR_MESSAGE}.")
+        endif()
+    endif()
+
+    set(downloaded_source_files "")
+
+    foreach(key_value IN ZIP_LISTS minpack_source_basenames minpack_sha256_sums_for_source_files)
+        set(minpack_fortran_file ${key_value_0})
+        set(minpack_fortran_file_sha256 ${key_value_1})
+        set(minpack_fortran_file_url "${minpack_url}/${minpack_fortran_file}")
+        set(minpack_fortran_file_destination "${destination_dir}/${minpack_fortran_file}")
+
+        if (EXISTS ${minpack_fortran_file_destination})
+            message(STATUS " Found fortran file (${minpack_fortran_file}) at")
+            message(STATUS "     ${minpack_fortran_file_destination}")
+        else()
+            message(STATUS " Downloading fortran file (${minpack_fortran_file})")
+            message(STATUS "     with expected SHA256 sum ${minpack_fortran_file_sha256}")
+
+            file(DOWNLOAD
+                ${minpack_fortran_file_url}
+                ${minpack_fortran_file_destination}
+                EXPECTED_HASH SHA256=${minpack_fortran_file_sha256}
+            )
+
+            list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
+            if (NOT (${STATUS_CODE} EQUAL 0))
+                list(GET DOWNLOAD_STATUS 1 ERROR_MESSAGE)
+                message(FATAL_ERROR "Failed to download ${minpack_fortran_file}: ${ERROR_MESSAGE}.")
+            endif()
+        endif()
+
+        list(APPEND downloaded_source_files ${minpack_fortran_file_destination})
+    endforeach()
+    message(STATUS "")
+    message(STATUS " Download completed.")
+    message(STATUS "")
+
+    set(${minpack_source_files} "${downloaded_source_files}" PARENT_SCOPE)
+    set(${minpack_copyright} "${minpack_copyright_file_destination}" PARENT_SCOPE)
+endfunction()
